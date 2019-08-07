6b45604818cd4257c5341abd5d8e520b50ccddca
polinausheva123 <53872655+polinausheva123@users.noreply.github.com>
2019-08-07 23:28:53 +0300

    Add files via upload

---
+++ 01.Dictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" | ").ToList();
+
+            var wordsAndDefinitions = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var tokens = input[i].Split(": ").ToList();
+
+                string word = tokens[0];
+                string definition = tokens[1];
+
+                if (!wordsAndDefinitions.ContainsKey(word))
+                {
+                    wordsAndDefinitions.Add(word, new List<string>());
+
+                } //if its not in the list
+
+
+                wordsAndDefinitions[word].Add(definition);
+
+
+            }
+
+            var commands = Console.ReadLine().Split(" | ").ToList(); //bit | developer | programmer
+
+            for (int i = 0; i < commands.Count; i++) //bit
+            {
+                if (wordsAndDefinitions.ContainsKey(commands[i]))
+                {
+                    Console.WriteLine(commands[i]);
+
+                    foreach (var definition in wordsAndDefinitions[commands[i]].OrderByDescending(x => x.Length))
+                    {
+                        Console.WriteLine(" -" + definition);
+                    }
+
+                }
+            }
+
+            string finalCommand = Console.ReadLine();
+
+            if (finalCommand == "List")
+            {
+                List<string> allKeys = new List<string>();
+
+                foreach (var kvp in wordsAndDefinitions.OrderBy(x => x.Key))
+                {
+                    allKeys.Add(kvp.Key);
+                }
+
+                Console.WriteLine(string.Join(" ", allKeys));
+            }
+        }
+    }
+}
---
+++ 02.Deciphering.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        var encrypted = Console.ReadLine();
+        var substrings = Console.ReadLine().Split(' ').ToArray();
+
+
+        Regex rx = new Regex("(^[d-z#{}|]+)");
+
+        string decrypted = null;
+
+        if (rx.IsMatch(encrypted))
+        {
+            for (int i = 0; i < encrypted.Count(); i++)
+            {
+                var curLetter = encrypted[i];
+                var reduced = curLetter - 3;
+                decrypted += (char)reduced;
+
+            }
+
+
+            var decryptedExtra = decrypted.Replace(substrings[0], substrings[1]);
+            Console.WriteLine(decryptedExtra);
+        }
+        else
+        {
+            Console.WriteLine("This is not the book you are looking for.");
+        }
+
+    }
+
+}
